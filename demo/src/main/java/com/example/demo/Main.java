e2ae52699800514642c913cda1ea8a18fcdf256b
Alicia Hernández <ahernandez@uc-3-312.AluVedruna.local>
2024-09-18 19:03:35 +0200

    Agregar programa para leer archivo de texto

--- demo/src/main/java/com/example/demo/Main.java
+++ demo/src/main/java/com/example/demo/Main.java
@@ -1,8 +1,25 @@
 package com.example.demo;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        lecturaArchivo (url="C:\\carpetaejemplo\\miArchivo.txt");;
     }
+
+
+    
+    public static void lecturaArchivo()throws IOException{ // excepcion usando java.IO
+        try{
+        Path file = Path.of(url); // ruta al fichero
+        String content = Files.readString(file); // el contenido del fichero a String con el método readString
+        System.out.println(content); // imprime el contenido 
+        }catch(IOException e){
+            System.out.println("El archivo no ha sido encontrado");
+        }
+    }
+
 }
+
 
